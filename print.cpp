c90be5fa54f972e9cd48faea7d92f4204799e04f
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-05-24 22:55:09 +0600

    Basic CPP

---
+++ print.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    // std :: cout << "Hello World";
+
+    int x = 10;
+    char c = 'A';
+    double d = 24.89;
+    
+    //Printing New Space(<<" "<<) and New Line(std :: endl);
+    std :: cout << x << " " << c << std :: endl << d << std :: endl; 
+
+    // Printing text;
+    std :: cout << "My Favourite Number " << x;
+
+    return 0;
+}
