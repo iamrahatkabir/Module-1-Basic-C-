659468cb1445a6cf2c2c91931d78b58ab91f027b
Ifty MyAlice <iftymyalice@Iftys-MacBook-Air.local>
2025-05-25 07:35:19 +0600

    Switch

---
+++ switch_7.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int day;
+    
+    cin >> day;
+
+    switch(day)
+    {
+        case 1: 
+            cout << "Saturday\n";
+            break;
+
+        case 2:
+            cout << "Sunday\n";
+            break;
+
+        case 3:
+            cout << "Monday\n";
+            break;
+
+        default:
+            cout << "Invalid input\n";
+
+    }
+
+
+    return 0;
+}
