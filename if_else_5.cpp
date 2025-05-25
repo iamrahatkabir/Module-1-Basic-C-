9f3852baa263f108d5fcafd955491f2bc1c12b32
Ifty MyAlice <iftymyalice@Iftys-MacBook-Air.local>
2025-05-25 06:39:51 +0600

    If Else in C++

---
+++ if_else_5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 13;
+
+    if(x%2 == 0)
+    {
+        cout << "Even" << endl;
+    }
+    else 
+    {
+        cout << "Odd" << endl;
+    }
+
+    return 0;
+}
