a5024533f7ebc052e18610e1d47c0d6c53c3f922
Ifty MyAlice <iftymyalice@Iftys-MacBook-Air.local>
2025-05-25 06:27:37 +0600

    Print fixed character in double after .

---
+++ setprecision.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+
+int main()
+{
+
+    double d = 4.89890;
+
+    cout << fixed << setprecision(5) << d << endl;
+
+    return 0;
+}
